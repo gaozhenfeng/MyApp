e0f87e6ddfe17e0e279b19900d967f7519c1c6ff
好阵风 <444731400@qq.com>
2017-09-12 11:42:24 +0800

    修改22

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+        Log.e("TAG","第二此次该");
         initView();
         initData();
     }
